792ff08af7805a954408f82b02edf6856ff19ded
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-20 12:24:23 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,18 @@
+package com.basicsinjava;
+
+public class Main {
+    public static void printSum(int n, int i, int sum){
+        if(i==n){
+            sum+=n;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        printSum(n, i+1, sum);
+    }
+
+    public static void main(String args[]){
+        int n=3;
+        printSum(n, 1, 0);
+    }    
+}
