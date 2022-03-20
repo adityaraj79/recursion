98d5e3b77ef1ac996d69e0bff95ac56c8dff2c41
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-20 11:37:03 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,16 @@
+package com.basicsinjava;
+
+public class Main {
+    public static void printNumb(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printNumb(n-1);
+    }
+
+    public static void main(String args[]){
+        int n=5;
+        printNumb(n);
+    }
+}
